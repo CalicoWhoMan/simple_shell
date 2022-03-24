d33d9b959261303d83085c0871979414024a383a
CalicoWhoMan <mikeyshrier@gmail.com>
2022-03-24 11:13:43 -0700

    maind shell file

--- main.c
+++ main.c
@@ -8,19 +8,24 @@
 int main(void)
 {
 	int bootup = isatty(STDIN_FILENO);
-	int b_name;
-	char *NOLL = NULL; 
-	size_t waiting = 0;
-	size_t n = 0;
+	int b_name;/*shell name*/
+	char *NOLL = NULL;/*the string from user*/ 
+	size_t waiting = 0;/*user input*/
+	size_t n = 0; /**/
 
 	while (1 != 0)
 	{
-	if (bootup == 1)
-	b_name = write(STDOUT_FILENO, "$YOU_DA_MAN ", 12); /*shell name*/
-	{
-	if (bootup == -1)
-	printf("Error");
-	}
-	waiting = getline(&NOLL, &n, stdin);
-	}
+		if (bootup == 1)
+		b_name = write(STDOUT_FILENO, "$YOU_DA_MAN ", 12); /*shell name*/
+		{
+			if (bootup == -1)
+			printf("Error");
+		}
+		waiting = getline(&NOLL, &n, stdin);/*wait for user input*/
+		if (waiting == EOF)
+		{
+			break; /*getline error*/
+		}
+		/*need to handle the args in the string, serperate them*/
+		
 }
